abeadf646183e7999fe4a07e6612dc2dd5a9688c
DumaArtem <155986240+DumaArtem@users.noreply.github.com>
2024-01-10 21:49:05 +0200

    Delete all folders

--- Tables/brand.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE `brand` (
-  `BrandId` INT NOT NULL AUTO_INCREMENT,
-  `BrandName` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (BrandId)
-)
-ENGINE = INNODB,
-CHARACTER SET utf8mb4,
-COLLATE utf8mb4_0900_ai_ci;
